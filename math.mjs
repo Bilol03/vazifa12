ee7b9ca1f79495883a06910b5d5ecf3d9c34647a
Bilol <madiyarovbilol78@gmail.com>
2025-02-04 07:50:01 +0500

    Birinchi vazifa bajarildi

---
+++ math.mjs
@@ -0,0 +1,7 @@
+export let multiply = (num1, num2) =>  {
+    return num1 * num2
+}
+
+export let divide = (num1, num2) => {
+    return num1 / num2
+}
---
+++ result.js
@@ -0,0 +1,4 @@
+let math = require('./math.js')
+
+console.log(math.add(2, 3))
+console.log(math.subtract(5, 2))
